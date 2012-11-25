dc27ad90b03edc7c385b021eceef16e261f9578e
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2012-11-25 16:38:53 +0100

    Added domain module

---
+++ <application>.domain/src/main/java/<application>/exception/ApplicationException.java
@@ -0,0 +1,15 @@
+package <application>.exception;
+
+public class ApplicationException extends RuntimeException {
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(Exception ex) {
+        super(ex);
+    }
+
+    public ApplicationException(String message, Exception ex) {
+        super(message, ex);
+    }
+}
